70e8b67324a214a15f6465e9e06cd84711b6d81e
Adam Kidd <adamkidd3@gmail.com>
2021-12-15 14:23:15 +0000

    first commit, first three tests passing

---
+++ models/cinema.js
@@ -0,0 +1,17 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.getTitles = function(){
+  return this.films.map((film) => film.title);
+};
+
+Cinema.prototype.findByTitle = function(title){
+  return this.films.find((film) => film.title === title);
+};
+
+Cinema.prototype.filterByGenre = function(genre){
+  return this.films.filter((film) => film.genre === genre); 
+};
+
+module.exports = Cinema;
